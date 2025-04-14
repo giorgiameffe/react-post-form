a7831ad307eda214953b8eb713730f89e1874dcc
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-14 16:54:23 +0200

    feat: install axios, add useState and OnChange event function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+// Hook
+import { useState } from "react";
+
 function App() {
+
+  // variabile di stato che corrisponde ad un oggetto
+  const [formPost, setFormPost] = useState({
+    author: '',
+    title: '',
+    body: '',
+    public: false
+  })
+
+  // funzione unica per la gestione dell'evento OnChange dei campi
+  function handleFormPost(event) {
+
+    setFormPost(formPost => ({
+      ...formPost,
+      [event.target.name]: event.target.value
+    }))
+  }
 
   return (
     <>
+      <h1>React Post Form</h1>
 
+      <div>
+        <label htmlFor="author-element">Autore </label>
+        <input
+          type="text"
+          id="author-element"
+          name="author"
+          value={formPost.author}
+          onChange={handleFormPost}
+          placeholder="Nome dell'autore" />
+      </div>
     </>
   )
 }
