7fb616c2ec9b2198a48cc458cfc0efc1cf39cedf
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-14 17:31:06 +0200

    feat: add form and function to create a new post

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,81 @@ function App() {
   // funzione unica per la gestione dell'evento OnChange dei campi
   function handleFormPost(event) {
 
+    const checkboxValue = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+
     setFormPost(formPost => ({
       ...formPost,
       [event.target.name]: event.target.value
     }))
+
   }
+
+  // funzione per creare nuovo post tramite chiamata all'API
+  function createPost(event) {
+    event.preventDefault();
+    console.log('Ho creato un nuovo post');
+  }
+
+  //
 
   return (
     <>
       <h1>React Post Form</h1>
+      <form onSubmit={createPost}>
+        <div>
+          <label htmlFor="author-element">Autore del post </label>
+          <input
+            type="text"
+            id="author-element"
+            name="author"
+            value={formPost.author}
+            onChange={handleFormPost}
+            placeholder="Nome dell'autore" />
+        </div>
 
-      <div>
-        <label htmlFor="author-element">Autore </label>
-        <input
-          type="text"
-          id="author-element"
-          name="author"
-          value={formPost.author}
-          onChange={handleFormPost}
-          placeholder="Nome dell'autore" />
-      </div>
+        <hr />
+
+        <div>
+          <label htmlFor="title-element">Titolo del post </label>
+          <input
+            type="text"
+            id="title-element"
+            name="title"
+            value={formPost.title}
+            onChange={handleFormPost}
+            placeholder="Titolo del post" />
+        </div>
+
+        <hr />
+
+        <div>
+          <label htmlFor="body-element">Contenuto del post </label>
+          <input
+            type="text"
+            id="body-element"
+            name="body"
+            value={formPost.body}
+            onChange={handleFormPost}
+            placeholder="Contenuto del post" />
+        </div>
+
+        <hr />
+
+        <div>
+          <label htmlFor="public-element">Post pubblico? </label>
+          <input
+            type="checkbox"
+            id="public-element"
+            name="public"
+            checked={formPost.public}
+            onChange={handleFormPost} />
+        </div>
+
+        <hr />
+
+        <button>Crea post</button>
+      </form>
+
     </>
   )
 }
